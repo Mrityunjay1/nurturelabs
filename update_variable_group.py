1ce1d6dcbf7b4a556ee86d3213b4cdc991bcbf2c
Mrutunjay Singh <34768330+Mrityunjay1@users.noreply.github.com>
2022-02-18 15:41:10 +0530

    Add files via upload

---
+++ update_variable_group.py
@@ -0,0 +1,41 @@
+import requests
+import json
+from datetime import date
+
+service_name = 'Parmeet-Service'
+stage1_variable_group = 'Stage_1_Variable_Group'
+
+url = "https://dev.azure.com/sukritibhandari/Parmeet%20Testing/_apis/distributedtask/variablegroups?api-version=6.0-preview.2"
+headers = {
+    'Authorization': 'Basic cGFybWVldC5zLnNpbmdoQHB3Yy5jb206YWt1M3d5cW5neW0yeHNxdmFlb3d1a3RlZmg2aHp0d2locnhwd2ZwZ29pdnd4dGJsdnR3YQ==',
+    'Content-Type': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, verify=False).json()
+
+# print(response)
+
+stage1_variable_group_id = 0
+for i in response['value']:
+    # print(i['id'],i['name'])
+    if(i['name'] == stage1_variable_group):
+        stage1_variable_group_id = i['id']
+
+print(stage1_variable_group, stage1_variable_group_id, end='\t')
+
+url = "https://dev.azure.com/sukritibhandari/Parmeet%20Testing/_apis/distributedtask/variablegroups/"+str(stage1_variable_group_id)+"?api-version=6.0-preview.1"
+
+payload = json.dumps({
+    "id": stage1_variable_group_id,
+    "type": "Vsts",
+    "name": stage1_variable_group,
+    "variables": {
+        stage1_variable_group+'-'+service_name: {
+            "value": str(date.today())
+        }
+    }
+})
+
+response = requests.request("PUT", url, headers=headers, data=payload, verify=False).json()
+
+print(response)
